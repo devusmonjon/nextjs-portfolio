2ca68f4627bf44b9a222ad9b254ee76a301333c5
usmonjon <umediagram@gmail.com>
2024-06-23 21:50:36 +0500

    fixed

--- src/components/testimonials/testimonials.tsx
+++ src/components/testimonials/testimonials.tsx
@@ -3,7 +3,10 @@ import { testimonials } from "@/data/constants";
 
 const Testimonials = () => {
     return (
-        <section className="w-full py-16 md:py-20 2xl:py-24 bg-gray-50">
+        <section
+            className="w-full py-16 md:py-20 2xl:py-24 bg-gray-50"
+            id="testimonials"
+        >
             <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 px-4 md:gap-12 md:px-8">
                 <div className="flex flex-col items-center gap-4">
                     <div className="sefl-center">
