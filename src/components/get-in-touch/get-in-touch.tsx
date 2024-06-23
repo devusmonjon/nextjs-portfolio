a5525aa899781243e3cae1079c56bf68eb420a83
usmonjon <umediagram@gmail.com>
2024-06-23 21:49:34 +0500

    fixed

--- src/components/get-in-touch/get-in-touch.tsx
+++ src/components/get-in-touch/get-in-touch.tsx
@@ -21,7 +21,10 @@ const GetInTouch = () => {
     const router = useRouter();
 
     return (
-        <section className="w-full bg-gray py-16 md:py-20 2xl:py-24">
+        <section
+            className="w-full bg-gray py-16 md:py-20 2xl:py-24"
+            id="contact"
+        >
             <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 px-4 md:gap-12 md:px-8">
                 <div className="flex flex-col items-center gap-4">
                     <div className="self-center">
